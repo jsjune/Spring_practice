ff56438bfc52a5d4658c4927070f57d91801699f
jsjune <94538575+jsjune@users.noreply.github.com>
2022-03-19 14:43:07 +0900

    Delete week03_error/src directory
    delete

--- week03_error/src/main/java/com/example/week03/domain/MemoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.week03.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface MemoRepository extends JpaRepository<Memo, Long> {
-    List<Memo> findAllByOrderByModifiedAtDesc(); // findAll By Order By ModifiedAt Desc 다 찾아서 순서대로 정렬해라 수정된 날짜를 기준으로 내림차순(최신순)
-}
--- week03_error/src/main/java/com/example/week03/domain/MemoRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.week03.domain;
-
-import lombok.Getter;
-
-@Getter
-public class MemoRequestDto {
-    private String username;
-    private String contents;
-}
