2ac90466873367a85a4dfb4369cff469a277102d
jsjune <avraskio@gmail.com>
2022-03-24 14:01:55 +0900

    initial commit

---
+++ Board/src/main/java/com/sparta/board/WebMvcConfig.java
@@ -0,0 +1,29 @@
+package com.sparta.board;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class WebMvcConfig {
+
+    private final ObjectMapper objectMapper;
+
+    @Bean
+    public MappingJackson2HttpMessageConverter jsonEscapeConverter() {
+        ObjectMapper copy = objectMapper.copy();
+        copy.getFactory().setCharacterEscapes(new com.sparta.board.HTMLCharacterEscapes());
+        return new MappingJackson2HttpMessageConverter(copy);
+    }
+
+
+}
